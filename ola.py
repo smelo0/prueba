36726cc96944fe71764628c2302956157925c61c
Melo <smelocv@gmail.com>
2025-11-11 16:42:49 -0500

    Revert "este hueso sera para el branch prueba.exe"
    This reverts commit 74b01c44b96fbdd728c917dcf97506c4d916424b.

--- ola.py
+++ ola.py
@@ -1,10 +1,3 @@
 print("esto es un el gueso ")
 print("esto no es el hueso ")
-print(f'este es el hueso {"prueba.exe"} ')
-
-
-# algunos cambios aca
-
-# algunos cambiso aqui
-
-# mas cambios para probar mas funciones
+print("este es el hueso final ")
